f1532b128e1344d1a06a8a12becfe9488226b754
theau <theau.zatti@gmail.com>
2020-04-06 20:55:11 +0200

    Rendering update

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import sys
+from copy import deepcopy
 
 from tools import *
 
@@ -159,6 +160,21 @@ class Teeko:
         self.state = State().__random_start__()
         self.turn_to = randomChoice(self.state.players)
 
+        self.square_width = (SCREEN_SIZE[1] - 100) // GRID_SIZE
+
+        self.font = pygame.font.Font('Amatic-Bold.ttf', 50)
+        self.playerone = self.font.render('Player 1', True, BLACK)
+        self.playeronerect = self.playerone.get_rect()
+        self.playeronerect.center = (int((SCREEN_SIZE[0] - self.square_width * GRID_SIZE) / 4), 150)
+
+        self.playertwo = self.font.render('Player 2', True, BLACK)
+        self.playertworect = self.playertwo.get_rect()
+        self.playertworect.center = (
+        int(3 * (SCREEN_SIZE[0] - self.square_width * GRID_SIZE) / 4) + self.square_width * GRID_SIZE, 150)
+
+        self.backbtn = Button((SCREEN_SIZE[0] - self.square_width * GRID_SIZE) / 4 - 75, SCREEN_SIZE[1] - 80, 150, 50,
+                              '< Back', BACKGROUND)
+
     #  move = (0, (pos token à placer), (0, 0)) ou (1, (pos token à deplacer), (direction))
     def minMax(self, move, current_state, depth, alpha, beta, maximizing_player, primary_player_idt):
         new_state = deepcopy(current_state)
@@ -246,14 +262,10 @@ class Teeko:
 
         for j in range(GRID_SIZE):
             for i in range(GRID_SIZE):
-                pygame.draw.circle(self.surf, RED if self.grid[j][i] == 2 else BLACK,((i * self.square_width + self.square_width // 2)+int((SCREEN_SIZE[0]-self.square_width*GRID_SIZE)/2), j * self.square_width + self.square_width // 2+int((SCREEN_SIZE[1]-self.square_width*GRID_SIZE)/2)),TOKEN_RADIUS, TOKEN_THICKNESS if self.grid[j][i] == 0 else 0)
+                pygame.draw.circle(self.surf, RED if self.state.grid[j][i] == 2 else BLACK,((i * self.square_width + self.square_width // 2)+int((SCREEN_SIZE[0]-self.square_width*GRID_SIZE)/2), j * self.square_width + self.square_width // 2+int((SCREEN_SIZE[1]-self.square_width*GRID_SIZE)/2)),TOKEN_RADIUS, TOKEN_THICKNESS if self.state.grid[j][i] == 0 else 0)
 
     def parse_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            if self.backbtn.on_button(pos):
-                return CODE_TO_MENU
-
+        pass
 
     def print(self):
         print(self.grid)
