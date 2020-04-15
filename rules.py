5938fbdd9e82f498cdf03b907703c85ac0f498d2
Xwaler <bemonys@gmail.com>
2020-04-15 17:31:31 +0200

    Rules changed again

--- rules.py
+++ rules.py
@@ -3,22 +3,22 @@ import pygame
 from constants import *
 from views import Button
 
+
 class Rules:
-    def __init__(self,surf):
+    def __init__(self, surf):
         self.surf = surf
         self.font = pygame.font.Font('Amatic-Bold.ttf', 120)
         self.title = self.font.render('Teeko Rules', True, BLACK)
         self.title_rect = self.title.get_rect()
         self.title_rect.center = (SCREEN_SIZE[0] / 2, 130)
 
-        with open('rules.txt','r') as f:
+        with open('rules.txt', 'r') as f:
             self.rulestxt = f.readlines()
-
 
         self.font = pygame.font.Font('Amatic-Bold.ttf', 40)
 
         self.leavebtn = Button((SCREEN_SIZE[0] - 200) / 2, (SCREEN_SIZE[1] - 50) / 2 + 300, 200, 50, 'Leave',
-                                BACKGROUND)
+                               BACKGROUND)
 
     def parseEvent(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -26,7 +26,6 @@ class Rules:
 
             if self.leavebtn.on_button(pos):
                 return CODE_TO_MENU
-
 
     def render(self):
         self.surf.fill(BACKGROUND)
@@ -37,10 +36,10 @@ class Rules:
             self.leavebtn.drawRect(self.surf)
 
         self.surf.blit(self.title, self.title_rect)
-        i=0
+        i = 0
         for lines in self.rulestxt:
             line = self.font.render(lines, True, BLACK)
             line_rect = line.get_rect()
-            line_rect.center = (SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2 - 100 + i*70 )
-            self.surf.blit(line,line_rect)
-            i+=1
+            line_rect.center = (SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2 - 100 + i * 70)
+            self.surf.blit(line, line_rect)
+            i += 1
